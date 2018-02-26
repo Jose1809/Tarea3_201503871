423280025030bfcf3947843342869f01d8814f5c
Jose <Jose@192.168.1.11>
2018-02-25 22:05:37 -0600

    Tarea3 Inicializada

---
+++ src/tarea3/Tarea3.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tarea3;
+
+import java.util.Scanner;//con esto podemos ejecutar la funcion escaner
+
+/**
+ *
+ * @author Jose
+ */
+public class Tarea3 {
+
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Menu llamada1=new Menu();//llamamos a la clase (en este caso es Menu())
+        llamada1.menu();//llamamos al metodo de la clase Menu() y este metodo en este caso es menu()
+        
+        
+    }
+
+}
